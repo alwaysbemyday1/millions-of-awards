407790626791a0185749c69b7e5d5ff4b2f56f09
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-03-16 14:51:36 +0900

    UserManager Created

--- server/core/models.py
+++ server/core/models.py
@@ -3,8 +3,30 @@ from django.contrib.auth.models import  AbstractBaseUser, BaseUserManager, Permi
 from django.utils import timezone
 
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+   
+    def create_user(self, nickname, password, **extra_fields):
+        if not nickname:
+            raise ValueError('nickname does not exist')
+        if not password:
+            raise ValueError('password does not exist')
+        
+        email = self.normalize_email(email)
+        user = self.model(nickname=nickname ,email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email = self.normalize_email(email), password=password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 class User(AbstractBaseUser, PermissionsMixin):
-#    objects = UserManager()
+   objects = UserManager()
    
    nickname = models.CharField(max_length=25, unique=True)
    email = models.EmailField(max_length=255, unique=True, null=True, blank=True)
