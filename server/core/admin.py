ae70b135b9a3ca47d54a3b9401ba4e83f0b1a8fe
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-03-17 01:08:42 +0900

    Add Admins

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, MajorCategory, MinorCategory, AwardsInfo
 
 
 class UserAdmin(admin.ModelAdmin):
     fields = ['email', 'username']
 
+class MajorCategoryAdmin(admin.ModelAdmin):
+    fields = ['__all__']
+
+class MinorCategoryAdmin(admin.ModelAdmin):
+    fields = ['__all__']
+
+class AwardsInfoAdmin(admin.ModelAdmin):
+    fields = ['__all__']
+
 admin.site.register(User, UserAdmin)
+admin.site.register(MajorCategory, MajorCategoryAdmin)
+admin.site.register(MinorCategory, MinorCategoryAdmin)
+admin.site.register(AwardsInfo, AwardsInfoAdmin)
