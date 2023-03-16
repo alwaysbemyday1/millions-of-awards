3d51f11760f9411d0e69c0c9b20e916a297cb670
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-03-16 14:50:39 +0900

    Model_User Created

--- server/core/models.py
+++ server/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import  AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+#    objects = UserManager()
+   
+   nickname = models.CharField(max_length=25, unique=True)
+   email = models.EmailField(max_length=255, unique=True, null=True, blank=True)
+   is_active = models.BooleanField(default=True)
+   is_admin = models.BooleanField(default=False)
+
+   USERNAME_FIELD = 'nickname'
+
+   def __str__(self):
+       return self.nickname
+
+   @property
+   def is_staff(self):
+       return self.is_admin
