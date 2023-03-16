8dbb204af67250e2d67c1ffd9bfeadc63e1d672b
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-03-16 20:49:40 +0900

    Create Model_Categories

--- server/core/models.py
+++ server/core/models.py
@@ -1,8 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 from .managers import UserManager
+
+class AwardsInfo(models.Model):
+    name = models.CharField(max_length=255)
+
+class MajorCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name_en = models.CharField(max_length=25)
+    name_ko = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return f"{self.name_en} / {self.name_ko}"
+
+class MinorCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    major_category = models.ForeignKey(MajorCategory, on_delete=models.CASCADE)
+    name_en = models.CharField(max_length=25)
+    name_ko = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return f"({self.major_category.name_ko}) {self.name_en} / {self.name_ko}"
 
 class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
