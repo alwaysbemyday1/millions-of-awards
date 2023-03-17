860723bf967adc77535c32aa178d7f841a165430
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-03-18 01:06:51 +0900

    Modify Line about Renamed Models

--- server/core/viewsets/awards_viewset.py
+++ server/core/viewsets/awards_viewset.py
@@ -10,7 +10,7 @@ from core.serializers.nominee_serializer import NomineeSerializer
 class AwardsViewSet(ModelViewSet):
     queryset = Awards.objects.all()
     serializer_class = AwardsSerializer
-    awards_queryset = Ceremony.objects.all()
+    ceremony_queryset = Ceremony.objects.all()
     ceremony_serializer = CeremonySerializer
     nominee_queryset = Nominee.objects.all()
     nominee_serializer = NomineeSerializer
@@ -29,14 +29,14 @@ class AwardsViewSet(ModelViewSet):
 
     @action(methods=['get'], detail=True, url_path=r'(?P<ceremony_index>[^/.]+)')
     def several_awards(self, request, name, ceremony_index):
-        queryset = self.awards_queryset.filter(awards__name=name, index=ceremony_index)
+        queryset = self.ceremony_queryset.filter(awards__name=name, index=ceremony_index)
         serializer = self.ceremony_serializer(queryset, many=True)
         return Response(serializer.data)
 
     @action(methods=['get'], detail=True, url_path=r'(?P<ceremony_index>[^/.]+)/nominees')
     def several_awards_nominees(self, request, name, ceremony_index):
-        awards = self.awards_queryset.filter(awards__name = name, index=ceremony_index)
-        awards_id = awards.values_list('id', flat=True)
-        queryset = self.nominee_queryset.filter(awards=awards_id[0])
+        ceremony = self.ceremony_queryset.filter(awards__name = name, index=ceremony_index)
+        ceremony_id = ceremony.values_list('id', flat=True)
+        queryset = self.nominee_queryset.filter(ceremony=ceremony_id[0])
         serializer = self.nominee_serializer(queryset, many=True)
         return Response(serializer.data)
